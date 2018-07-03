a898cf0a9e3213b2b8e80b5905777945788e4a00
krishnknit <krishnknit@gmail.com>
2018-07-03 10:40:04 +0530

    increase the performance

--- comboMaster.py
+++ comboMaster.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+
 import os
 import sys
 import time
@@ -10,6 +11,7 @@ import argparse
 import datetime
 import threading
 import pandas as pd
+import numpy as np
 from itertools import combinations
 
 
@@ -17,7 +19,11 @@ class ComboMaster(object):
 
 	def __init__(self):
 		''' Initialise the class with required valiables '''
+
 		self.finalCombMinDF = pd.DataFrame(columns=['combfmlyid', 'scenario_dt', 'numbyden'])
+		self.fdf_display=pd.DataFrame(columns=['bus_dt','fmly_comb','scenario_dt','combtotal_def','combtotal_cfr','covern_multdef'])
+		self.printingDF=pd.DataFrame(columns=['bus_dt','fmly_id','scenario_dt','defcny_nooffset','cover1','fmly_cfr','comb_def','comb_cfr','calc_val','num_fmly'])
+		self.df_table_insert=pd.DataFrame(columns=['bus_dt', 'scenario_dt', 'fmly_id', 'lastupdates'])
 		self.csvPath = os.getcwd()
 		self.fmlyDict = {}
 		self.combDict = {}
@@ -26,6 +32,13 @@ class ComboMaster(object):
 		self.numbyden = []
 		self.comblen = []
 		self.keyCounts = 0
+		self.target=0.5
+		self.final_target=1.0
+		self.start_bus_dt='20180101'
+		self.start_bus_dt='20180102'
+		self.comb_len=[]
+		self.comb_fltr=[]
+		self.dist_bus_dt=[]		
 
 
 	def getargs(self):
@@ -65,8 +78,7 @@ class ComboMaster(object):
 							  WHERE bus_dt = '{}' '''.format(business_dt)
 				
 				self.df2 = pd.read_sql(sqlstr_2, con)
-				#print self.df1
-				#print self.df2
+				self.fmlyDict.clear()
 				for index, row in self.df1.iterrows():
 					self.fmlyDict.setdefault(row['scenario_dt'], []).append(row['fmly_id'])
 
@@ -74,54 +86,37 @@ class ComboMaster(object):
 			logging.error("readData(), e: {}".format(e))
 		else:
 			## close database connection
-			con.close()
-			self.finalCombMinDF = pd.DataFrame(columns=['combfmlyid', 'scenario_dt', 'numbyden'])
-			self.fmlyDict = {}
-			self.combDict = {}
-			self.finalDict = {}
-			self.scenario_fml_val = []
-			self.numbyden = []
-			self.comblen = []
-			self.keyCounts = 0
-			self.createCombination()
 
 
-	def generateComb(self, data, kcnt):
-		comb = combinations(data, kcnt)
-		for cc in comb:
-			yield cc
-
-	def newGetCombinations(self, data, key, kcnt):
+	def newGetCombinations(self, data, key, kcnt,bus_dt):
 		''' get the combinations for correct data '''
 		try:
-			fdf_temp = pd.DataFrame(columns=['fmly_comb', 'scenario_dt', 'combtotal_def', 'combtotal_cfr', 'covern'])
+			fdf_temp = pd.DataFrame(columns=['bus_dt','fmly_comb', 'scenario_dt', 'combtotal_def', 'combtotal_cfr', 'covern_multdef'])
 			total_cfr = self.df2['fmly_cfr'].sum()
-			genObj = self.generateComb(data, kcnt)
-			for cc in genObj:
-				combtotal_def = 0
-				combtotal_cfr = total_cfr
-				covern = 0.0
-				for c in cc:
-					compVal = self.df3.loc[(self.df3['scenario_dt'] == key) & (self.df3['fmly_id'] == c), 'def']
-					if compVal.empty:
-						continue
-					combtotal_def += compVal.iloc[0, 0]
-					combtotal_cfr -= compVal.iloc[0, 1]
+			combtotal_def = 0
+			combtotal_cfr = total_cfr
+			covern_multdef = 0.0
+			for c in data:
+				compVal = self.df3.loc[(self.df3['scenario_dt'] == key) & (self.df3['fmly_id'] == c), ['def',fmly_cfr]]
+				if compVal.empty:
+					continue
+				combtotal_def += compVal.iloc[0, 0]
+				combtotal_cfr -= compVal.iloc[0, 1]
 
-				if combtotal_cfr != 0:
-					covern = float(combtotal_def)/combtotal_cfr
+			if combtotal_cfr != 0:
+				covern_multdef = float(combtotal_def)/combtotal_cfr
 
-				if covern - self.target < 0.01:
-					self.fdf_temp = self.fdf_temp.append(
-								{'combfmlyid': list(cc), 
-								'scenario_dt': key, 
-								'combtotal_def': combtotal_def,
-								'combtotal_cfr': combtotal_cfr}, ignore_index=True)
+			if covern_multdef > self.target:
+				self.fdf = self.fdf.append(
+							{'bus_dt':bus_dt
+							'fmly_comb': list(cc), 
+							'scenario_dt': key, 
+							'combtotal_def': combtotal_def,
+							'combtotal_cfr': combtotal_cfr, 
+							'covernmult_def':covern_multdef}, ignore_index=True)
 
-				if len(self.fdf_temp) >= 5:
-					self.fdf = self.fdf.append(fdf_temp)
-					fdf_temp.drop(fdf_temp.index, inplace=True)
-					break
+			self.fdf.sort_values(['covern_multdef'],ascending=[False],inplace=True)
+			self.fdf=self.fdf.iloc[0]	
 		except Exception as e:
 			logging.error("newGetCombinations(), e: {}".format(e))
 
@@ -154,30 +149,21 @@ class ComboMaster(object):
 		''' function to create combination all fmly_id uniquely '''
 
 		try:
-			self.fdf = pd.DataFrame(columns=['fmly_comb', 'scenario_dt', 'combtotal_def', 'combtotal_cfr'])
+			self.fdf = pd.DataFrame(columns=['fmly_comb', 'scenario_dt', 'combtotal_def', 'combtotal_cfr','covern_multdef'])
 			self.fdf_comb = pd.DataFrame(columns=['fmly_comb'])
 			self.reducedDF = pd.DataFrame(columns=['combfmlyid', 'scenarioDate', 'cover1', 'calcVal'])
+			temp_list=[]
 						
 			for key in self.fmlyDict.keys():
-				keyCounts = self.getReducedFmlyIds(self.fmlyDict[key], key, self.df1	)
-				self.newGetCombinations(self.fmlyDict[key], key, keyCounts + 1)
+				keyCounts, fmlyIds, temp_scenario_date = self.getReducedFmlyIds(self.fmlyDict[key], key)
+				if keyCounts!=0:
+					temp_list.append(tuple([keyCounts,temp_scenario_date]))
+					min_comb,min_scenario_dt=min(temp_list,key=lambda item:item[0])
+                        
+				if min_comb!=0:
+					self.newgetCombinations(fmlyIds,min_scenario_dt,min_comb+1,bus_dt)
 
-			## generate numerator/denomenator
-			self.generateNumbyDen()
-			#print self.fdf
-			self.fdf['scenario_fml_val'] = self.scenario_fml_val
-			self.fdf['numbyden'] = self.numbyden
-			self.fdf['comblen'] = self.comblen
-
-			#print self.fdf
-			#finalDf =  self.fdf.loc[(self.fdf['numbyden'] != 'na') & (self.fdf['combfilter'] != 'na'), ['combfmlyid', 'scenario_dt', 'numbyden']]
-			finalDf =  self.fdf.loc[(self.fdf['numbyden'] != 'na'), ['combfmlyid', 'scenario_dt', 'numbyden', 'comblen']]
-			#print finalDf
-			
-			self.generateMinComb(finalDf)
-			self.generateCSVFile()
-		except Exception as e:
-			logging.error("createCombination(), e: {}".format(e))
+			self.fdf_display=self.fdf_display.appennd(self.fdf,ignore_index=True)
 
 
 	def getReducedFmlyIds(self, fmlyIds, scen_date):
@@ -193,14 +179,17 @@ class ComboMaster(object):
 					df1_tmp['comb_def'] = gd.get_group(scen_date)['defcny_no_offset'].transform(pd.series.cumsum)
 					df1_tmp['comb_cfr'] = total_cfr - gd.get_group(scen_date)['fmly_cfr'].transform(pd.series.cumsum)
 					df1_tmp['calc_val'] = df1_tmp['comb_def']/df1_tmp['comb_cfr']
+					df1_tmp['comb_fml'] = gd.get_group(scen_date)['fmly_id'].apply(lambda x: str(x) + ' ')cumsum()
 					df1_tmp.dropna(inplace=True)
 					df1_tmp = df1_tmp.reset_index()
 					break
 		
 			if df1_tmp['calc_val'].max() > self.target:
-				return df1_tmp[df1_tmp.calc_val > self.target].index[0]
+				fmlid = df1_tmp[df1_tmp.calc_val > self.target]['comb_fml'].values.split(' ')
+				fmllength = df1_tmp[df1_tmp.calc_val > self.target].index[0]
+				return fmllength, fmlid, scen_date
 			else:
-				return 0
+				return 0,'9999-99-99'
 		except Exception as e:
 			logging.error("getReducedFmlyIds(), e: {}".format(e))
 
@@ -310,21 +299,55 @@ class ComboMaster(object):
 			logging.error("createDataToExcel(): unable to write excel file, e: {}".format(e))
 		else:
 			logging.info("excel file: {} has created ...".format(excelFile))
+        
 
+	def exec_stored_proc(self):
+		con,cur=getconnection()
+		sql_exec=''' exec p_reverse ? ?;'''
+		values=(20180101,20180102)
+		cur.execute(sql_exec,values)
+		cur.commit
+		con.close
 
 
 	def writeData_conn(self):
+		engine:sqlachemy.engine.base.Emgine=None
+		conn.sqlachemy.engine.connection=None
 		params = urllib.parse.quote_plus(conn_str)
 		engine = create_engine("mssql+pyodbc://?odbc_connect=%s" % params)
 		conn = engine.connect()
 		try:
-			res = conn.execute("select count(*) from gsd_scenario_famly where bus_dt between '{}' and '{}'".format(start_dt, end_dt))
+			res = conn.execute('''SELECT COUNT(*) 
+								  FROM gsd_scenario_famly 
+								  WHERE bus_dt 
+								  BETWEEN '{}' AND '{}' '''.format(start_dt, end_dt))
 			if res:
-				conn.execute("DELETE from gsd_scenario_famly where bus_dt between '{}' and '{}'".format(start_dt, end_dt))
+				conn.execute('''DELETE FROM gsd_scenario_famly 
+								WHERE bus_dt 
+								BETWEEN '{}' AND '{}' '''.format(start_dt, end_dt))
 
 			self.df_table_insert.to_sql(name='gsd_scenario_famly', con=conn, schema='dbo', if_exists='append', index=False)
 		except Exception as e:
 			logging.error("writeData_conn(): insertion failed, e: {}".format(e))
+			conn.close()
+
+
+	def gatherData():
+		temp_dates=pd.DataFrame()
+		con,cur=getconnection()
+		sqlstr_bus_dt = '''	SELECT DISTINCT bus_dt 
+							FROM g_cover1 
+							WHERE bus_dt 
+							BETWEEN {} AND {} ORDER BY bus_dt ASC
+						'''.format(self.start_bus_dt,self.end_bus_dt)
+		try:
+			temp_dates=read_sql(sqlstr_bus_dt,con)
+			dist_bus_dt=temp_dates.iloc[:,0].tolist()
+			for bus_dt in dist_bus_dt:
+				self.readData(bus_dt)
+				self.createcombination(bus_dt)
+		except Exception as e:
+			logging.error("gatherData(): unable to gather data, e: {}".format(e))
 
 
 	def main(self):
@@ -336,9 +359,14 @@ class ComboMaster(object):
 		logging.info("starting combo master process ...")
 		logging.info("********************************************************")
 		stime = time.time()
-		self.getargs()
-		self.readData()
-		#self.createCombination()
+		#self.getargs()
+		self.gatherData()
+		for index,row in self.fdf_display.iterrows():
+			for id in row['fmly_comb']:
+				self.df_table_insert=self.df_table_insert.append({'bus_dt':row['bus_dt'],'scenario_dt':eow['scenario_dt'],'fmly_id':id})
+		
+		self.exec_stored_proc()
+		self.writedata_conn()
 		self.createDataToExcel()
 		etime = time.time()
 		ttime = etime - stime		
